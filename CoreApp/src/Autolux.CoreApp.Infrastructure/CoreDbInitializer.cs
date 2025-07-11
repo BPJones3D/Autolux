bb1c2195778a7239411afb55b05346954268429e
Peter Jones <peter.jones@loyaltylogistix.com>
2025-07-11 19:23:26 +0100

    add CoreDbInitializer

---
+++ CoreApp/src/Autolux.CoreApp.Infrastructure/CoreDbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using Autolux.CoreApp.Infrastructure.Seeds;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Autolux.CoreApp.Infrastructure;
+public class CoreDbInitializer(CoreDbContext _dbContext, IConfiguration conf)
+{
+    public async Task SeedAsync()
+    {
+        await SeedCars();
+    }
+
+    private async Task SeedCars()
+    {
+        if (!await _dbContext.Cars.AnyAsync())
+        {
+            var awardRules = CarSeed.GetCars();
+            _dbContext.Cars.AddRange(awardRules);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
