f33c6a47b198eb035e8b5afbcee84f22e74f3ed4
Peter Jones <peter.jones@loyaltylogistix.com>
2025-08-31 14:44:20 +0100

    add shared kernel with base entity and permission keys

---
+++ Common/src/Autolux.SharedKernel/BaseClasses/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Autolux.SharedKernel.BaseClasses;
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+    public bool IsDeleted { get; private set; }
+
+    public DateTime Created { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime Modified { get; set; }
+    public Guid ModifiedBy { get; set; }
+
+    public void MarkAsDeleted()
+    {
+        IsDeleted = true;
+    }
+}
